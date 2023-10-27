93d999743d84f27f1029d072d23aba5156660060
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-10-27 13:45:52 +0700

    Penambahan function untuk memilih status nikah di CobaViewModel

--- app/src/main/java/com/example/dataform/CobaViewModel.kt
+++ app/src/main/java/com/example/dataform/CobaViewModel.kt
@@ -18,18 +18,27 @@ class CobaViewModel: ViewModel() {
         private set
     var jenisKl: String by mutableStateOf("")
         private set
+    var email: String by mutableStateOf("")
+        private set
+    var status: String by mutableStateOf("")
+        private set
     private val _uiState = MutableStateFlow(DataForm())
     val uiState: StateFlow<DataForm> = _uiState.asStateFlow()
 
-    fun insertData(nm: String, tlp: String, almt: String, jk: String){
+    fun insertData(nm: String, tlp: String, almt: String, jk: String, ema: String, stat: String){
         namaUsr = nm;
         noTlp = tlp;
         namaAlmt = almt
         jenisKl = jk;
-
+        email   = ema;
+        status  = stat;
     }
 
     fun setJenisK(pilihJK: String){
         _uiState.update { currentState -> currentState.copy(sex = pilihJK) }
+    }
+
+    fun setNikah(pilihStat: String){
+        _uiState.update { currentState -> currentState.copy(stats = pilihStat) }
     }
 }
